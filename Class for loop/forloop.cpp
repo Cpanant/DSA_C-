ca69460f2004acf2106b505c6c927ba399f2d9f6
CP Anant <96227238+Cpanant@users.noreply.github.com>
2022-07-23 20:58:57 +0530

    Add files via upload

---
+++ Class for loop/forloop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"We will print Aditya from 1 to 1000 using for loop;"<<endl;
+    for(int i=1;i<=1000;i++)
+    {
+        cout<<" Aditya;"<<i<<endl;
+    }
+    return 0;
+}
